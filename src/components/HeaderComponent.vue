38562a339d98b3f02a4b7b14dc1de3c241a42c1e
Gleb Mizghulin <glebmiz14@gmail.com>
2024-02-03 17:56:42 +0200

    v1

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="container header__container">
+      <div class="header__logo-wrapper">
+        <div class="header__logo">
+          <img src="/img/logo.png" alt="Logo">
+        </div>
+        <div class="header__info">
+          <b>Gleb Mizghulin</b>
+          <p>Full Stack Developer</p>
+        </div>
+      </div>
+      <div class="header__social-wrapper">
+        <a href="https://t.me/glebmiz"><font-awesome-icon :icon="['fab', 'telegram']" /></a>
+        <a href="viber://chat/?number=0937221016"><font-awesome-icon :icon="['fab', 'viber']" /></a>
+        <a href="https://www.linkedin.com/in/gleb-miz-830305291"><font-awesome-icon :icon="['fab', 'linkedin']" /></a>
+        <a href="https://www.upwork.com/freelancers/~013f8dfff64cb36be0"><font-awesome-icon :icon="['fab', 'upwork']" /></a>
+      </div>
+    </div>
+  </header>
+  
+</template>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+@import '../assets/scss/breakpoints.scss';
+
+.header {
+  position: fixed;
+  width: 100%;
+  height: 120px;
+  padding: $spacing-md;
+  z-index: 999;
+}
+
+.header__container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: $spacing-md;
+  height: 100%;
+
+  @include breakpoint('xs') {
+   flex-direction: row;
+  }
+}
+
+.header__logo-wrapper{
+  display: flex;
+  gap: $spacing-md;
+  align-items: center;
+  flex-direction: space-between;
+
+  @include breakpoint('xs') {
+    justify-content: start;
+  }
+}
+
+.header__logo {
+  height: 50px;
+  border-radius: 50%;
+
+  img {
+    height: 100%;
+    margin-top: -2px;
+  }
+}
+
+.header__info p{
+  font-size: $size-xs;
+}
+
+.header__social-wrapper {
+  display: flex;
+  gap: $spacing-xxl;
+  align-items: center;
+  
+  @include breakpoint('xs') {
+    margin-left: auto;
+    justify-content: start;
+    gap: $spacing-xl;
+  }
+}
+</style>
---
+++ src/components/SwiperComponent.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, defineProps } from 'vue';
+const props = defineProps(['images']);
+const images = ref(props.images || []);
+</script>
+
+<template>
+    <swiper-container :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :slidesPerView="'auto'"
+        :coverflowEffect="{
+            rotate: 40,
+            stretch: 1,
+            depth: 10,
+            modifier: 1,
+            slideShadows: true,
+        }" :pagination="true" class="projects-swiper">
+        <swiper-slide v-for="(image, index) in images" :key="index">
+            <img :src="image.src" :alt="image.src">
+            <div class="swiper__text">{{ image.text }}</div>
+        </swiper-slide>
+
+    </swiper-container>
+</template>
+
+<style lang="scss">
+ .projects-swiper .swiper{
+    padding-bottom: 10px;
+}
+.projects-swiper swiper-slide {
+    width: 70%;
+
+    img {
+        width: 100%;
+    }
+}
+.swiper__text{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+</style>
